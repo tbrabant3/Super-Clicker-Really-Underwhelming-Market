2cb2bc65ba298d63207df187f3e279d4ad26bc77
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-15 15:50:30 -0500

    Removed unused import.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import Home from "./sections/home";
 import {createMuiTheme, CssBaseline} from "@material-ui/core";
 import {ThemeProvider} from "@material-ui/styles";

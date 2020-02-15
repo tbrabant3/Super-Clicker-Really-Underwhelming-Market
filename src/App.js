0ca26d565d396139b74ec264d7f07873ff687a32
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-15 15:11:36 -0500

    Seperated and finalized menu buttons.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Home from "./sections/home";
 import {createMuiTheme, CssBaseline} from "@material-ui/core";
 import {ThemeProvider} from "@material-ui/styles";
 
+// This theme overrides the palette's default theme to dark;
+// the themeProvider in the component bellow will be used to provide
+// theming to each of it's child components.
 const theme = createMuiTheme({
     palette: {
         type: 'dark'
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,15 @@
 import AppBar from "@material-ui/core/AppBar";
-import {Toolbar} from "@material-ui/core";
+import {Drawer, ListItemIcon, ListItemText, Toolbar} from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, {Fragment, useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import AddShoppingCartRoundedIcon from "@material-ui/icons/AddShoppingCartRounded";
+import ExposureRoundedIcon from "@material-ui/icons/ExposureRounded";
+import LibraryBooksRoundedIcon from '@material-ui/icons/LibraryBooksRounded';
 
 // This function creates different classes,
 // or groups of styles that we can put on
@@ -20,17 +25,47 @@ const Header = () => {
     // Here we initialize the classes that we are going to use.
     const classes = useStyles();
 
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+
+    const DrawerButton = ({text, icon}) =>
+        (
+            <ListItem button>
+                <ListItemIcon>
+                    {icon}
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+            </ListItem>
+        );
+
+    const LeftDrawer = () => (
+        <div
+            role={'presentation'}
+            onClick={toggleDrawer}>
+            <List>
+                <DrawerButton text={'Home'} icon={<AddShoppingCartRoundedIcon/>}/>
+                <DrawerButton text={'Upgrades'} icon={<ExposureRoundedIcon/>}/>
+                <DrawerButton text={'Inventory'} icon={<LibraryBooksRoundedIcon/>}/>
+            </List>
+        </div>
+    );
+
     return (
-        <AppBar position={'static'}>
-            <Toolbar>
-                <IconButton edge={'start'} color={'inherit'} className={classes.menuButton} aria-label={'menu'}>
-                    <MenuRoundedIcon/>
-                </IconButton>
-                <Typography variant={'h6'}>
-                    {'Home'}
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <Fragment>
+            <Drawer open={drawerOpen} onClose={toggleDrawer}>
+                <LeftDrawer/>
+            </Drawer>
+            <AppBar position={'static'}>
+                <Toolbar>
+                    <IconButton edge={'start'} color={'inherit'} className={classes.menuButton} aria-label={'menu'} onClick={toggleDrawer}>
+                        <MenuRoundedIcon/>
+                    </IconButton>
+                    <Typography variant={'h6'}>
+                        {'Home'}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Fragment>
     );
 };
 

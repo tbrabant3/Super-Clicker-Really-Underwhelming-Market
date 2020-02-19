70319d4397e94f02cffe056917343de24258a263
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-19 13:12:50 -0500

    Pulled out content section.

--- src/components/clicker/index.js
+++ src/components/clicker/index.js
@@ -3,7 +3,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import PageLayout from "../page-layout";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
@@ -19,19 +18,24 @@ const ClickableImage = () =>
     const [count, setCount] = useState(0);
     const incrementCounter = () => setCount(count + 1);
 
+    const ContentSection = () =>
+        (
+            <Grid container justify={'center'}>
+                <Grid container item justify={'center'} xs={12}>
+                    <img src={require('../../images/murdermart.png')} alt="murdermart"/>
+                </Grid>
+                <Grid container item justify={'center'} xs={12}>
+                    <Typography variant={'h5'}>
+                        { count }
+                    </Typography>
+                </Grid>
+            </Grid>
+        );
+
     return (
         <Card className={classes.root} onClick={incrementCounter}>
             <CardContent>
-                <Grid container justify={'center'}>
-                    <Grid container item justify={'center'} xs={12}>
-                        <img src={require('../../images/murdermart.png')} alt="murdermart"/>
-                    </Grid>
-                    <Grid container item justify={'center'} xs={12}>
-                        <Typography variant={'h5'}>
-                            { count }
-                        </Typography>
-                    </Grid>
-                </Grid>
+                <ContentSection/>
             </CardContent>
         </Card>
     )

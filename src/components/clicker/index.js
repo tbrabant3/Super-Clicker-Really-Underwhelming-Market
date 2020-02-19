ea33617031f8274c1f94d428c64ecd4757b38238
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-19 13:03:37 -0500

    Centered content.

--- src/components/clicker/index.js
+++ src/components/clicker/index.js
@@ -3,6 +3,8 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import PageLayout from "../page-layout";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -20,10 +22,16 @@ const ClickableImage = () =>
     return (
         <Card className={classes.root} onClick={incrementCounter}>
             <CardContent>
-                <img src={require('../../images/murdermart.png')} alt="murdermart"/>
-                <Typography variant={'h5'}>
-                    { count }
-                </Typography>
+                <Grid container justify={'center'}>
+                    <Grid container item justify={'center'} xs={12}>
+                        <img src={require('../../images/murdermart.png')} alt="murdermart"/>
+                    </Grid>
+                    <Grid container item justify={'center'} xs={12}>
+                        <Typography variant={'h5'}>
+                            { count }
+                        </Typography>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     )

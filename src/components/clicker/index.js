ef9747d81df8ab6e3f7e921c136f8818d0dea75f
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-26 01:18:23 -0500

    Removed unused import.

--- src/components/clicker/index.js
+++ src/components/clicker/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import ClickButton from './button';
 import { incrementCouponCount } from '../../redux/actions';

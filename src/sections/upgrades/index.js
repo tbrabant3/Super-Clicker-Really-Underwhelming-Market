1bf5ecdd5b62fd12f83349b63e7a101a6b3977c2
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-28 13:51:17 -0500

    Changed magic numbers to constants.

--- src/sections/upgrades/index.js
+++ src/sections/upgrades/index.js
@@ -2,6 +2,10 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import CouponsPerClick from '../../components/upgrades-grid/coupons-per-click';
 import CouponsPerSecond from '../../components/upgrades-grid/coupons-per-second';
+import {
+	COUPONS_PER_CLICK_SECTION_MINIMUM,
+	COUPONS_PER_SECOND_SECTION_MINIMUM
+} from '../../constants/upgrades';
 
 const mapStateToProps = state => ({
 	coupons: state.CouponsReducer.coupons
@@ -10,8 +14,8 @@ const mapStateToProps = state => ({
 const Upgrades = ({ coupons }) => {
 	// The component Hierarchy to return.
 
-	const shouldShowCouponsPerClick = coupons > 19;
-	const shouldShowCouponsSecond = coupons > 999;
+	const shouldShowCouponsPerClick = coupons > COUPONS_PER_CLICK_SECTION_MINIMUM;
+	const shouldShowCouponsSecond = coupons > COUPONS_PER_SECOND_SECTION_MINIMUM;
 
 	return (
 		<Fragment>

551f6706e1a7745149e1b45c564ba9c344b48a31
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-04-03 14:23:28 -0400

    Added button tests, improved modal theming.

--- src/components/footer/GithubButton.js
+++ src/components/footer/GithubButton.js
@@ -1,11 +1,25 @@
 import React, { useState, Fragment } from 'react';
 import { GitHub } from '@material-ui/icons';
 import { Button, Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+	paper: {
+		position: 'absolute',
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(2, 4, 3),
+		top: '50%',
+		left: '50%'
+	}
+}));
 
 /**
  * @type {React.FC<>}
  */
-const GitHubButton = () => {
+const GithubButton = () => {
+	const classes = useStyles();
+
 	const [open, setOpen] = useState(false);
 	const toggle = () => setOpen(!open);
 	/**
@@ -33,10 +47,10 @@ const GitHubButton = () => {
 				Github
 			</Button>
 			<Modal open={open} onClose={toggle}>
-				<div>Thank for looking at our project!</div>
+				<div className={classes.paper}>Thank for looking at our project!</div>
 			</Modal>
 		</Fragment>
 	);
 };
 
-export default GitHubButton;
+export default GithubButton;
--- src/components/footer/index.test.js
+++ src/components/footer/index.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Footer from './index';
-import GitHubButton from './githubButton';
+import GithubButton from './GithubButton';
 import { mount } from 'enzyme';
 import { Button, Modal } from '@material-ui/core';
+import AboutButton from '../AboutButton';
+import CreditsButton from '../CreditsButton';
 
 describe('Footer Testing', () => {
 	const { open } = window;
@@ -17,14 +19,14 @@ describe('Footer Testing', () => {
 		window.open = open;
 	});
 
-	test('Should Render Footer', () => {
+	it('Should Render Footer', () => {
 		const component = renderer.create(<Footer />);
 		const tree = component.toJSON();
 		expect(tree).toMatchSnapshot();
 	});
 
-	test('Should Load Github Page', () => {
-		const wrapper = mount(<GitHubButton />);
+	it('Should Load Github Page', () => {
+		const wrapper = mount(<GithubButton />);
 		const githubButton = wrapper.find(Button);
 		const githubModal = wrapper.find(Modal);
 		expect(githubModal.prop('open')).toBe(false);
@@ -35,5 +37,23 @@ describe('Footer Testing', () => {
 			'https://github.com/tbrabant3/Super-Clicker-Really-Underwhelming-Market',
 			'_blank'
 		);
+	});
+
+	it('Should Render the About Modal', () => {
+		const wrapper = mount(<AboutButton />);
+		const githubButton = wrapper.find(Button);
+		const githubModal = wrapper.find(Modal);
+		expect(githubModal.prop('open')).toBe(false);
+		githubButton.simulate('click');
+		expect(githubModal.prop('open')).toBe(true);
+	});
+
+	it('Should Render the Credits Modal', () => {
+		const wrapper = mount(<CreditsButton />);
+		const githubButton = wrapper.find(Button);
+		const githubModal = wrapper.find(Modal);
+		expect(githubModal.prop('open')).toBe(false);
+		githubButton.simulate('click');
+		expect(githubModal.prop('open')).toBe(true);
 	});
 });
